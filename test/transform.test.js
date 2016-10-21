37992b3926c5041a42799c7aa8c39b3c62bd7e8b
Christopher Bruner <chrbruner9@gmail.com>
2016-10-21 16:18:20 -0700

    working MVP - added before condition on test suite to make sure fubarred is deleted before making a new one

--- test/transform.test.js
+++ test/transform.test.js
@@ -1,10 +1,23 @@
 var assert = require('chai').assert;
 var fs = require('fs');
-var transform = require('./transform');
+var transform = require('../transform');
+var rimraf = require('rimraf');
 
 describe('transform', done => {
-    it('makes sure console logging works :)', () => {
-        console.log('console logging is good');
+    before(done => {
+        fs.access('./fubarred.bmp', (err) => {
+            rimraf('./fubarred.bmp', (errTwo) => {
+                if (errTwo) done(errTwo);
+                else done();
+            });
+        });
+    });
+    
+    it('makes sure console logging works :)', done => {
+        setTimeout(function() {
+            console.log('console logging to make sure rimraf is working');
+            done();
+        }, 1000);
     });
 
     it('runs the invertColors function once', done => {
@@ -18,9 +31,9 @@ describe('transform', done => {
                 if(err) done(err);
                 for(var i = 0; i <= buffer.length; i++){
                     assert.equal(buffer[i], bufferTwo[i]);
-                }
+                };
                 done();
-            })
-        })
+            });
+        });
     });
 });
--- transform.js
+++ transform.js
@@ -1,14 +1,10 @@
 const fs = require('fs');
-// const path = require('path');
 
 function invertColors(dir, cb){
     fs.readFile(dir, (err, buffer) => {
         if (err) return cb(err);
-        //console.log("Done reading bitmap");
         startPoint = buffer.readInt32BE(11);
-        //console.log('buffer size ', buffer.length);
         for (let i = startPoint; i <= buffer.length; i++) {
-            //console.log('current buffer ', i);
             buffer[i] = (255 +(buffer[i]*-1));
             if (i === buffer.length) {
                 fs.writeFile('fubarred.bmp', buffer, (err) => {
@@ -17,16 +13,7 @@ function invertColors(dir, cb){
                 });
             };  
         };
-        // console.log('__dirname: ' +  __dirname)
-        // fs.writeFile('/' + __dirname + '/../test/' + 'fubarred.bmp', buffer);
-        //console.log('fs.writeFile called from transform.js');
     });
 };
-// invertColors('../non-palette-bitmap.bmp');
+
 module.exports = invertColors;
-
-
-// it('makes buffer from string', done => {
-//     const buffer = Buffer.from('the quick brown fox', 'ascii');
-//     console.log(buffer);
-// )};
