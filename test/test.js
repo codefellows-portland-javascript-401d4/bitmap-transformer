6b258def9823eea53833afcd24005c561dffc416
Nathan Hugon <nthugon@gmail.com>
2016-10-22 14:44:54 -0700

    added test to verify transformed bitmap file exists

--- test/test.js
+++ test/test.js
@@ -1,8 +1,31 @@
 const read = require('../lib/read.js');
 const assert = require('assert');
 const fs = require('fs');
+const rimraf = require('rimraf');
 
 describe('testing transformation of bitmap', function() {
+
+  beforeEach(function(done) {
+    rimraf('./inverted.bmp', done);
+    console.log('rimraffed');
+  });
+
+  it('adds a transformed image', function(done) {
+    function addTest() {
+      console.log('addTest running');
+      assert.ok(fs.existsSync('./inverted.bmp'));
+      done();
+    };
+
+    read.readWrite('./non-palette-bitmap.bmp', addTest);
+
+
+    // console.log('adding image test running');
+    // read.readWrite('.non-palette-bitmap.bmp', () => {
+    //   assert.ok(fs.existsSync('./inverted.bmp'));
+    // });
+    // done();
+  });
 
 
   it('inverts the colors', function(done) {
@@ -12,7 +35,7 @@ describe('testing transformation of bitmap', function() {
       done();
     }
 
-    read.readBitFile(invertTest);
+    read.readWrite('./non-palette-bitmap.bmp', invertTest);
 
 
 
