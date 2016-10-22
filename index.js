3ff715cd0b03c4f312c040d37d950d08f65dda63
Gloria Guy <totallygloria@gmail.com>
2016-10-21 23:46:28 -0700

    After a lot of searching, finally found some math on using alpha and intensity to desaturate an image. Anholt mentioned that the RGB are expected to be a value normed between 0-1 (rather than 255). Got it to work! Now have two filters, which can be used via command line with flags.

--- index.js
+++ index.js
@@ -7,15 +7,18 @@ const readBMP = require('./lib/readBMP');
 cmd.version('1.0.0')
   .usage('[options] <bmp file ...>')
   .option('-i, --invert', 'Invert the colors in a bitmap')
+  .option('-g, --grayscale', 'Desaturate the bitmap into grayscale (based on intensity)')
   .parse(process.argv);
-
 
 var bmpFile = process.argv[process.argv.length - 1];
 var bmpPath = path.join(bmpFile);
 
-readBMP(bmpPath, errorMsg);
+readBMP(bmpPath, errorHandler);
 
 
-function errorMsg(err) {
+function errorHandler(err) {
   console.error('Error: ', err);
 }
+
+exports.invert = cmd.invert;
+exports.grayscale = cmd.grayscale;
