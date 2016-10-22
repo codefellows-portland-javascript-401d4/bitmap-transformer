4dc05b34f3fe23ab52e6028b472d5f24c6e60a99
Gloria Guy <totallygloria@gmail.com>
2016-10-21 20:45:12 -0700

    Started the skeleton for using commander for command line flags for different types of transformations -- I'd like to have several transformations and call that function based on the flag passed in. Doesn't work yet, but some of the skeleton is there.

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const fs = require('fs');
 const path = require('path');
-const buf = require('buffer');
-const Bmp = require('./lib/readBMP');
+const cmd = require('commander');
+const readBMP = require('./lib/readBMP');
 
 
+cmd.version('1.0.0')
+  .usage('[options] <bmp file ...>')
+  .option('-i, --invert', 'Invert the colors in a bitmap')
+  .parse(process.argv);
 
 
-bmpFile = process.argv[process.argv.length - 1];
-bmpPath = path.join(bmpFile);
+var bmpFile = process.argv[process.argv.length - 1];
+var bmpPath = path.join(bmpFile);
 
-Bmp.readBMP(bmpPath, cb);
+readBMP(bmpPath, errorMsg);
 
 
-function cb(err) {
-  console.log('you got an error: ', err);
+function errorMsg(err) {
+  console.error('Error: ', err);
 }
