971a5b7f0952ce1f74228c07b3de2f6cb5af0dcf
David Chase <dchasepdx@gmail.com>
2016-10-23 15:37:23 -0700

    test passes on an inversion. Commented section is test for greyscale.

--- openFile.js
+++ openFile.js
@@ -1,12 +1,16 @@
 var fs = require('fs');
-fs.readFile('./non-palette-bitmap.bmp', function(err, buffer){
+fs.readFile('./palette-bitmap.bmp', function(err, buffer){
   if (err) throw err;
   var buffer = buffer;
   const buf = Buffer.from(buffer);
   var imageOffset = buf.readIntLE(10, 4);
   
-  console.log(buf.readIntLE(28, 4));
+  console.log('pixel depth',buf.readIntLE(28, 4));
   console.log('size', buf.readIntLE(2, 4));
+  console.log('colors in palette', buf.readIntLE(46, 4));
+  console.log('color palette starts', buf.readInt8(52));
+  console.log('color palette continues', buf.readInt8(53));
+
   console.log(imageOffset);
   console.log(buf.readInt8(63));
   console.log(buf.readInt8(64));
