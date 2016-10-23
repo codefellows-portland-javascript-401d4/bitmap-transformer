bdd62acdc5393636d3c5e70dfa0c519d1a8f6d52
Nathan Hugon <nthugon@gmail.com>
2016-10-23 11:20:06 -0700

    test to check inversion on byte complete and working

--- index.js
+++ index.js
@@ -3,19 +3,8 @@ const transform = require('./lib/transform.js');
 const write = require('./lib/write.js');
 const stream = require('stream');
 
-function callTransform() {
-  console.log('transform called');
-  transform.transformFile(callWrite);
+function callBack() {
+  console.log('callback of readWrite run');
 };
-function callWrite() {
-  console.log('write called');
-  write.writeFile(allDone);
-};
-function allDone() {
-  console.log('All Done');
 
-}
-
-
-
-// read.readFile();
+read.readWrite('./non-palette-bitmap.bmp', callBack);
--- test/test.js
+++ test/test.js
@@ -5,39 +5,51 @@ const rimraf = require('rimraf');
 
 describe('testing transformation of bitmap', function() {
 
+  //delete inverted image before each part of test
   beforeEach(function(done) {
     rimraf('./inverted.bmp', done);
     console.log('rimraffed');
   });
-
+  
+  //delete inverted image after whole test is done
+  after(function(done) {
+    rimraf('./inverted.bmp', done);
+    console.log('rimraffed');
+  });
+  
+  //test to see whether image is being added
   it('adds a transformed image', function(done) {
-    function addTest() {
+
+    function addTest() {   
       console.log('addTest running');
       assert.ok(fs.existsSync('./inverted.bmp'));
       done();
-    };
+    }
 
     read.readWrite('./non-palette-bitmap.bmp', addTest);
 
-
-    // console.log('adding image test running');
-    // read.readWrite('.non-palette-bitmap.bmp', () => {
-    //   assert.ok(fs.existsSync('./inverted.bmp'));
-    // });
-    // done();
   });
 
-
+  //test to see whether byte values are being inverted
   it('inverts the colors', function(done) {
 
     function invertTest() {
-      console.log('in the invertTest');
-      done();
+      fs.readFile('./non-palette-bitmap.bmp', function(err, data) {
+        if (err) throw err;
+        //get value of byte 15000
+        var byte15k = data[15000];
+        //compute inverted value of a byte
+        var invertedByte = 255 - byte15k;
+        //compare inverted value of byte 15000 to expected value
+        assert.deepEqual(invertedByte, read.byte15kInverted);
+        console.log('byte 15k:', byte15k);
+        console.log('expected inverted value:', invertedByte);
+        console.log('byte 15k inverted:', read.byte15kInverted);
+        done();
+      });
     }
 
     read.readWrite('./non-palette-bitmap.bmp', invertTest);
-
-
 
   });
 
